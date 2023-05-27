414d1acce68c952ea3714574c0c23314ba696c93
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-05-27 21:06:29 +0530

    main page temp ui done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import { space_grotesk } from "@/lib/fonts";
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
-  return <main>yo</main>;
+  return (
+    <main className="min-h-screen w-full flex flex-col gap-2 items-center justify-center select-none py-2 px-4">
+      <h1
+        style={space_grotesk.style}
+        className="tracking-tighter leading-tight text-3xl font-bold"
+      >
+        getid
+      </h1>
+      <Link
+        href="/dashboard"
+        className="items-center flex bg-white border-2 border-black duration-200 ease-in-out focus:outline-none hover:bg-black hover:shadow-none hover:text-primary-foreground justify-center rounded-xl shadow-[5px_5px_black] transform transition w-full lg:w-auto px-6 py-3"
+      >
+        Go To Dashboard <ChevronRight className="w-4" />
+      </Link>
+    </main>
+  );
 }
