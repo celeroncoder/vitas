c053daa5d2aeabf3a18edadf3b5a11fb1fda25c5
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-05-30 02:34:31 +0530

    added action dropdown to member-data-table

--- components/members-table/columns.tsx
+++ components/members-table/columns.tsx
@@ -2,9 +2,18 @@
 
 import { Member } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
 import { Checkbox } from "../ui/checkbox";
 import { DataTableColumnHeader } from "./column-header";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<Member>[] = [
   {
@@ -49,6 +58,37 @@ export const columns: ColumnDef<Member>[] = [
     accessorKey: "position",
     header: ({ column }) => {
       return <DataTableColumnHeader column={column} title="Position" />;
+    },
+  },
+
+  //   action col
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      // TODO: use this to copy the digital id image url
+      const member = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText("#")}
+            >
+              Copy URL
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
