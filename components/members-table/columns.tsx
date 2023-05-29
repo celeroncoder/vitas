495457abf33647ce3d3efedaddf7e431343850b8
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-05-30 02:09:12 +0530

    added sorting to members-data-table

--- components/members-table/columns.tsx
+++ components/members-table/columns.tsx
@@ -2,12 +2,42 @@
 
 import { Member } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
 
 export const columns: ColumnDef<Member>[] = [
-  { accessorKey: "id", header: "#" },
+  {
+    accessorKey: "id",
+    cell: ({ row }) => {
+      return (
+        <div className="text-right font-medium w-fit inline">
+          {row.getValue("id")}
+        </div>
+      );
+    },
+    header: ({ column }) => {
+      return (
+        <div
+          className="cursor-pointer"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          #
+        </div>
+      );
+    },
+  },
   {
     accessorKey: "name",
-    header: "Name",
+    header: ({ column }) => {
+      return (
+        <div
+          className="cursor-pointer inline-flex items-center"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 w-3" />
+        </div>
+      );
+    },
   },
   {
     accessorKey: "username",
--- components/members-table/data-table.tsx
+++ components/members-table/data-table.tsx
@@ -2,8 +2,10 @@
 
 import {
   ColumnDef,
+  SortingState,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
@@ -16,6 +18,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "../ui/button";
+import { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -26,10 +29,17 @@ export function DataTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
   });
 
   return (
