a740ff4439b07f90be30ccd2b8ecb3c040ae8cc0
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-06-11 19:41:43 +0530

    made the stripe webhook public

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ["/"] });
+export default authMiddleware({
+  publicRoutes: ["/", "/api/wehbhooks/stripe/"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
