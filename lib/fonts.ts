b77afed9b26b7c57aeed60f9c248b8a5ec4092fa
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-06-18 22:24:33 +0530

    feat: base email sending route configured

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,8 @@
 import { Inter, Space_Grotesk } from "next/font/google";
 
-export const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
 export const inter = Inter({ subsets: ["latin"] });
+export const space_grotesk = Space_Grotesk({
+  subsets: ["latin"],
+  preload: false,
+  fallback: ["sans-serif"],
+});
---
+++ pages/api/members/[id]/email.ts
@@ -0,0 +1,60 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { CardEmailSendProps } from "@/lib/validators";
+import { z } from "zod";
+import { service } from "@/service";
+import { getAuth } from "@clerk/nextjs/server";
+import { knock } from "@/lib/knock";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "POST") return res.status(405).end("Method not allowed.");
+
+  try {
+    const parsedId = z.string().safeParse(req.query.id);
+
+    if (!parsedId.success)
+      return res.status(400).json({ error: parsedId.error });
+
+    const id = parseInt(parsedId.data);
+
+    const member = await service.member.getOne(id);
+    if (!member) return res.status(404).json({ error: "Member not found" });
+
+    const project = await service.project.getOne(member.projectId);
+    if (!project) return res.status(404).json({ error: "Project not found" });
+
+    const auth = getAuth(req);
+
+    const projectOwnerName =
+      auth?.user?.firstName || auth?.user?.username || "Anonymous";
+
+    const props: CardEmailSendProps = {
+      cardUrl: `${
+        new URL(req.headers["origin"] || "http://localhost:3000").origin
+      }/card/${member.id}`,
+      member: {
+        name: member.name,
+      },
+      projectDisplayName: project.displayName,
+      projectOwnerName: projectOwnerName,
+    };
+
+    const { workflow_run_id } = await knock.workflows.trigger(
+      "get-id-card-delivery",
+      {
+        data: props,
+        recipients: [
+          {
+            id: member.id.toString(),
+            // TODO: replace with member.email once the member has email prop
+            email: "khushalbhardwaj0111@gmail.com",
+          },
+        ],
+      }
+    );
+
+    res.status(200).json({ workflow_run_id });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json(error);
+  }
+};
