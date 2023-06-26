d54fa21b96dec75d16295b819801e2eb9518ddb4
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-06-27 00:25:19 +0530

    improv: change the logo to the latest version

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,39 +6,39 @@ import { neobrutalism } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = {
-  title: "get.id",
-  description: "Generate ID Cards for your Team",
-  icons: "/favicon.ico",
+	title: "get.id",
+	description: "Generate ID Cards for your Team",
+	icons: "/favicon.png",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        style={inter.style}
-        className="scrollbar-thin scrollbar-thumb-rounded-sm scrollbar-thumb-lime-200/75 scrollbar-track-background scroll-smooth"
-      >
-        <ClerkProvider
-          appearance={{
-            baseTheme: neobrutalism,
-            variables: { colorPrimary: "#a3e635" },
-          }}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            storageKey="theme"
-          >
-            {children}
-            <Toaster />
-          </ThemeProvider>
-        </ClerkProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				style={inter.style}
+				className="scrollbar-thin scrollbar-thumb-rounded-sm scrollbar-thumb-lime-200/75 scrollbar-track-background scroll-smooth"
+			>
+				<ClerkProvider
+					appearance={{
+						baseTheme: neobrutalism,
+						variables: { colorPrimary: "#a3e635" },
+					}}
+				>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						storageKey="theme"
+					>
+						{children}
+						<Toaster />
+					</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
+	);
 }
