990d67f85a085672ae3437bc628457dbede76d83
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-07-05 22:38:59 +0530

    improv: moved auth pages to a route group and add a layout

---
+++ app/(auth)/sign-up/create-account/page.tsx
@@ -0,0 +1,17 @@
+import { CreateAccount } from "@/components/create-account";
+import { service } from "@/service";
+import { currentUser } from "@clerk/nextjs";
+
+import { redirect } from "next/navigation";
+
+export default async function CreateAccountPage() {
+	const user = await currentUser();
+
+	if (!user) redirect("/sign-in");
+
+	const account = await service.account.getOne(user.id);
+
+	if (account) redirect("/dashboard");
+
+	return <CreateAccount user={user} />;
+}
