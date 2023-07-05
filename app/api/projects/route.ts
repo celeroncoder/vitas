b887d1cff64da1e97fb3005dee1814d16e7469c7
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-07-06 02:46:06 +0530

    feat: migrated create-project to app-router

---
+++ app/api/projects/route.ts
@@ -0,0 +1,67 @@
+import { ProjectCreateProps } from "@/lib/validators";
+import { service } from "@/service";
+import { currentUser } from "@clerk/nextjs";
+
+// get all user's projects
+export async function GET(req: Request) {
+	const user = await currentUser();
+
+	if (!user) {
+		return new Response(
+			JSON.stringify({
+				error: "Unauthorized",
+			}),
+			{ status: 401 }
+		);
+	}
+
+	const projects = await service.project.getAll(user.id);
+
+	if (!projects) {
+		return new Response(
+			JSON.stringify({
+				error: "Couldn't fetch your projects",
+			}),
+			{ status: 404 }
+		);
+	}
+
+	return new Response(JSON.stringify(projects), { status: 200 });
+}
+
+// create account
+export async function POST(req: Request) {
+	try {
+		const parsedBody = ProjectCreateProps.safeParse(await req.json());
+
+		if (!parsedBody.success) {
+			return new Response(
+				JSON.stringify({
+					error: "Invalid Request, couldn't parse request body.",
+				}),
+				{ status: 400 }
+			);
+		}
+
+		const [success, project] = await service.project.create(parsedBody.data);
+
+		if (!success) {
+			return new Response(
+				JSON.stringify({
+					error: "Uh Oh! Some error occurred while creating project.",
+				}),
+				{ status: 500 }
+			);
+		}
+
+		return new Response(JSON.stringify(project), { status: 201 });
+	} catch (error) {
+		console.error(error);
+		return new Response(
+			JSON.stringify({
+				error: "Internal server error",
+			}),
+			{ status: 500 }
+		);
+	}
+}
