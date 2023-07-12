e3ebb792b0a342c164cc8d0936262ba7d3976da0
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-07-12 12:15:17 +0530

    feat: added react-query devtool

--- components/query-provider.tsx
+++ components/query-provider.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/lib/react-query";
 
 export const QueryProvider: React.FC<{ children: React.ReactNode }> = ({
 	children,
 }) => {
 	return (
-		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	);
 };
