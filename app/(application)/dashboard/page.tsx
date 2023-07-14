502994e2ae5c9b33ca4fd59ea99638b7a563f149
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2023-07-14 21:49:32 +0530

    improv: added muted text for no-projects...

--- app/(application)/dashboard/page.tsx
+++ app/(application)/dashboard/page.tsx
@@ -21,6 +21,13 @@ export default async function Dashboard() {
 				<CreateProjectForm />
 			</div>
 			{projects && <ProjectGrid projects={projects} />}
+			{projects.length <= 0 && (
+				<div className="relative w-full py-4 px-2">
+					<p className="text-center text-gray-500">
+						You don't have any projects yet. Create one now!
+					</p>
+				</div>
+			)}
 		</>
 	);
 }
